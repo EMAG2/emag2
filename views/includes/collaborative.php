efa990b04e24d9a9d6362518bc5123211720efd5
thiago <grupocodenome@gmail.com>
2017-09-18 01:57:20 -0300

    Separando módulos para facilitar manutenção no sistema.

---
+++ views/includes/collaborative.php
@@ -0,0 +1,15 @@
+<section>
+    <article class="content-grid p-xl-5">
+        <h5>And did you have that collaborative spirit?</h5>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+            laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+            dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+            delenit augue duis dolore te feugait nulla facilisi.</p>
+        <div class="mt-4 mb-5">
+            <button class="btn btn-outline-primary">Become a employee</button>
+            <button class="btn btn-primary">Become a sponsor</button>
+        </div>
+    </article>
+</section>
---
+++ views/includes/sponsor.php
@@ -0,0 +1,15 @@
+<section>
+    <article class="content-grid p-xl-5">
+        <h2>Meet some of our sponsors</h2>
+        <ul class="mt-5 pb-3">
+            <li class="float-left mr-5"><i><img src="assets/img/sponsors/google.png" alt=""></i></li>
+            <li class="float-left mr-5"><i><img src="assets/img/sponsors/microsoft.png" alt=""></i></li>
+            <li class="float-left mr-5"><i><img src="assets/img/sponsors/slack.png" alt=""></i></li>
+            <li class="float-left mr-5"><i><img src="assets/img/sponsors/digital-ocean.png" alt=""></i></li>
+            <li class="float-left mr-5" style="margin-top: -16px"><i><img src="assets/img/sponsors/jetbrain.png"
+                                                                          alt=""></i></li>
+            <li class="mr-5" style="margin-top: -16px"><i><img src="assets/img/sponsors/adobe.png" alt=""></i>
+            </li>
+        </ul>
+    </article>
+</section>
