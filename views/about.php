64dd6db2eade4efc1f58070e580a5eb77965ca8e
Thiago Cunha <grupocodenome@gmail.com>
2017-09-18 01:59:42 -0300

    Atualizando páginas e adicionando módulos do diretório include.

--- views/about.php
+++ views/about.php
@@ -37,40 +37,11 @@
             </article>
         </section>
 
-        <section>
-            <article class="content-grid p-xl-5">
-                <h2>Meet some of our sponsors</h2>
-                <ul class="mt-5 pb-3">
-                    <li class="float-left mr-5"><i><img src="assets/img/sponsors/google.png" alt=""></i></li>
-                    <li class="float-left mr-5"><i><img src="assets/img/sponsors/microsoft.png" alt=""></i></li>
-                    <li class="float-left mr-5"><i><img src="assets/img/sponsors/slack.png" alt=""></i></li>
-                    <li class="float-left mr-5"><i><img src="assets/img/sponsors/digital-ocean.png" alt=""></i></li>
-                    <li class="float-left mr-5" style="margin-top: -16px"><i><img src="assets/img/sponsors/jetbrain.png"
-                                                                                  alt=""></i></li>
-                    <li class="mr-5" style="margin-top: -16px"><i><img src="assets/img/sponsors/adobe.png" alt=""></i>
-                    </li>
-                </ul>
-            </article>
-        </section>
+        <!--    Collaborative section--!>
+        <?php require "includes/sponsor.php"; ?>
 
-
-        <!--    Collaborative section-->
-        <section>
-            <article class="content-grid p-xl-5">
-                <h5>And did you have that collaborative spirit?</h5>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                    laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                    ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
-                    dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla
-                    facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
-                    delenit augue duis dolore te feugait nulla facilisi.</p>
-                <div class="mt-4 mb-5">
-                    <button class="btn btn-outline-primary">Become a employee</button>
-                    <button class="btn btn-primary">Become a sponsor</button>
-                </div>
-            </article>
-        </section>
-    </div>
+        <!--    Collaborative section--!>
+        <?php require "includes/collaborative.php"; ?>
 
 
 </main>
--- views/home.php
+++ views/home.php
@@ -4,7 +4,7 @@
     </div>
     <h1 class="display-4 text-center mt-5">Global grid of magnetic anomaly</h1>
     <div class="text-center mt-4 mb-5">
-        <a class="btn btn-outline-primary mr-3" href="register">Register</a>
+        <a class="btn btn-outline-primary" href="register">Register</a>
         <a class="btn btn-primary" href="donation">Donation</a>
     </div>
 </main>
