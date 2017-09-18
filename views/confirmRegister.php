f31bb8ca17bd459c270823ef38ede17dfbe107cb
thiago <grupocodenome@gmail.com>
2017-09-18 01:57:55 -0300

    Adicionando a página de confirmação de registro de um usuário.

---
+++ views/confirmRegister.php
@@ -0,0 +1,19 @@
+<main class="container">
+    <div class="grid">
+        <section>
+            <article>
+                <div class="top">
+                    <h1 class="text-center mb-0">Thank you for registering on EMAG2!</h1>
+                    <p class="text-center mt-0 mb-0">Now you need to log into the system by <a href="login">clicking
+                            here!</a></p>
+                </div>
+            </article>
+        </section>
+
+        <!--    Collaborative section--!>
+        <?php require "assets/include/collaborative.php"; ?>
+
+    </div>
+
+
+</main>
