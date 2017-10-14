afea4ca325fe594112b3ae949ddb17a49ca39bc4
thiago <grupocodenome@gmail.com>
2017-10-14 12:29:48 -0300

    Adicionando senha para o servidor

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 require 'environment.php';
 
-define("BASE_URL", "http://localhost/emag2");
+define("BASE_URL", "http://binariux.com.br/emag2");
 
 
 global $config;
 $config = array();
 if (ENVIRONMENT == 'development') {
     $config['dbname'] = 'emag2';
-    $config['host'] = 'localhost:3306';
-    $config['dbuser'] = 'root';
-    $config['dbpass'] = '';
+    $config['host'] = 'localhost';
+    $config['dbuser'] = 'admin_thiago';
+    $config['dbpass'] = 'emag2_2017';
 } else {
     $config['dbname'] = 'emag2';
     $config['host'] = 'localhost:3306';
